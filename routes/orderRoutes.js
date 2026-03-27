7d55dd5a61d73647d1c86f3ae0323676dcd429f9
Bilal-Afzal-Bhatti <bilalafzalbhatti@gmail.com>
2026-03-28 01:49:36 +0500

    CREATED THE MODEL MY ORDER FOR GET THE USER ORDER HITORY

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -7,7 +7,7 @@ router.post("/create-checkout-session", createCheckoutSession);
 router.post("/cod", createCODOrder);
 router.put("/update-payment", paymentstatus);
 router.get("/track/:id",getOrderTracking);
-router.get('/user/:userId', getUserOrderHistory);
+router.get("/user/:userId",getUserOrderHistory);
 //.post("/", orderinfo);
 
 export default router;
