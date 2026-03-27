fe51c4925fe46c11827e2466732ac7760d42f864
Bilal-Afzal-Bhatti <bilalafzalbhatti@gmail.com>
2026-03-28 00:01:36 +0500

    deleted the server file

--- server.js
+++
@@ -1,7 +0,0 @@
-import app from "./app.js";
-const PORT = process.env.PORT || 5000;
-const HOST = "0.0.0.0";
-
-app.listen(PORT, HOST, () => {
-  console.log(`Server running at http://${HOST}:${PORT}/`);
-});
